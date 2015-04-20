4ca02f2cba5e06614c561ea513ae3358afc855d0
bc_rikko <s.shin1106@gmail.com>
2015-04-21 00:14:00 +0900

    リファクタリング 計算するときに文字列を数値の配列に変換してから行う

--- CodingDojo/src/KataBowling.ts
+++ CodingDojo/src/KataBowling.ts
@@ -1,27 +1,52 @@
 ﻿class Bowling {
+    /**
+     * ボウリングのスコアを計算する
+     * @param rolls ボウリングのスコア
+     * @return 1ゲームのスコア
+     */
     computeScore(rolls: string): number {
         var score = 0;
+        var rollList = this.convertScoreArray(rolls);
+
+        rollList.forEach((roll, i) => {
+            if (rolls[i] === 'X') {
+                // Strike
+                score += roll + rollList[i + 1] + rollList[i + 2];
+            } else if (rolls[i] === '/') {
+                // spare
+                score += roll + rollList[i + 1];
+            } else {
+                score += roll;
+            }
+        });
+
+        return score;
+    }
+
+    /**
+     * rollsを数値配列に変換する（記号も変換する）
+     * @param rolls ボウリングのスコア（文字列）
+     * @return 数値配列
+     */
+    private convertScoreArray(rolls: string): number[] {
+        var rollList: number[] = [];
 
         for (var i = 0; i < rolls.length; i++) {
             var roll = rolls[i];
-            if (roll === '-') {
-                // gutter
-                score += 0;
-
-            } else if (roll === 'X') {
+            if (roll === 'X') {
                 // strike
-                score += 10 + (+rolls[i + 1]) + (+rolls[i + 2]);
-
+                rollList.push(10);
             } else if (roll === '/') {
                 // spare
-                score += (10 - (+rolls[i - 1])) + (+rolls[i + 1]);
-
+                rollList.push(10 - rollList[i - 1]);
+            } else if (roll === '-') {
+                // gutter
+                rollList.push(0);
             } else {
-                // normal
-                score += +roll;
+                rollList.push(+roll);
             }
         }
 
-        return score;
+        return rollList;
     }
 }
