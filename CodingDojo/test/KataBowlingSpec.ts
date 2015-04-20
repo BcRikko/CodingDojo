87ef4d85c6112f1ffd48eb9ee6b7c07faae91559
bc_rikko <s.shin1106@gmail.com>
2015-04-20 21:59:43 +0900

    すべてガターの場合

--- CodingDojo/test/KataBowlingSpec.ts
+++ CodingDojo/test/KataBowlingSpec.ts
@@ -1,7 +1,12 @@
 ﻿/// <reference path="../scripts/typings/jasmine/jasmine.d.ts" />
 
-describe('ボウリングの投球',() => {
+describe('ボウリングのスコア計算',() => {
     beforeEach(() => {
         this.bowling = new Bowling();
+    });
+
+    it('すべてガターの場合',() => {
+        var result = this.bowling.computeScore('--------------------');
+        expect(result).toBe(0);
     });
 });
