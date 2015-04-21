d39bc0763eaa49622495e2d6a393a4c87e9a62b9
bc_rikko <s.shin1106@gmail.com>
2015-04-21 22:50:51 +0900

    引数がすべてEmptyの場合

--- CodingDojo/test/KataDictionaryReplacerSpec.ts
+++ CodingDojo/test/KataDictionaryReplacerSpec.ts
@@ -1,5 +1,12 @@
 ﻿/// <reference path="../scripts/typings/jasmine/jasmine.d.ts" />
  
 describe('KataDectionaryReplacer',() => {
+    beforeEach(() => {
+        this.replacer = new DictionaryReplacer();
+    });
 
+    it('引数がすべてEmptyの場合',() => {
+        var result = this.replacer.replace('', {});
+        expect(result).toBe('');
+    });
 });
