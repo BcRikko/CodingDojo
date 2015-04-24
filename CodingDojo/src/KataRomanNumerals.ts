0f4445dfbef9e4164956e1b3aba382786d9eacdd
bc_rikko <s.shin1106@gmail.com>
2015-04-24 14:04:38 +0900

    1を渡すとIが返ってくる

--- CodingDojo/src/KataRomanNumerals.ts
+++ CodingDojo/src/KataRomanNumerals.ts
@@ -1,5 +1,18 @@
-class KataRomanNumerals {
-    convert(value: number): string{
-        return '';
+export class KataRomanNumerals {
+    private romanNums = {
+        1: 'I',
+        2: 'II',
+        3: 'III',
+        4: 'IV',
+        5: 'V',
+        6: 'VI',
+        7: 'VII',
+        8: 'VIII',
+        9: 'IX',
+        10: 'X'
     };
+
+    convert(value: number): string {
+        return this.romanNums[value];
+    }
 }
--- CodingDojo/test/KataRomanNumeralsSpec.ts
+++ CodingDojo/test/KataRomanNumeralsSpec.ts
@@ -1,6 +1,14 @@
+/// <reference path="../src/KataRomanNumerals.d.ts" />
 /// <reference path="../scripts/typings/jasmine/jasmine.d.ts" />
 
-describe('KataRomanNumerals', ()=>{
-  
-  
+describe('KataRomanNumerals', () => {
+    beforeEach(() => {
+        this.romanNumerals = new KataRomanNumerals();
+    });
+
+    it('1を渡すとIが返ってくる', () => {
+        var result = this.romanNumerals.convert(1);
+        expect(result).toBe('I');
+    });
+
 });
